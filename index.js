aa8a650e28e01203d0b1dde0fa50c0d50ca947c8
John Ceci <tigeryan55@gmail.com>
2018-01-12 23:47:43 -0500

    package.json start

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const requestHandler = (request, response) => {
 	if (request.url != '/favicon.ico') {
 		async function run() {
 
-			const browser = await puppeteer.launch();
+			//const browser = await puppeteer.launch();
+			const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 			const page = await browser.newPage();
 
 			await page.goto('https://google.com/search?q=' + request.url);
