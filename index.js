07a48efb26d12cc0c142fa5ee0cc09fb3c1c9bc7
John Ceci <tigeryan55@gmail.com>
2018-01-12 20:25:59 -0500

    sort of working

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (request, response) => {
-	console.log(request.url);
-	response.end('Hello Node.js Server! ' + request.url);
 
 	const puppeteer = require('puppeteer');
 
+	console.log(request.url);
+
 	if (request.url != '/favicon.ico') {
 		async function run() {
+
 			const browser = await puppeteer.launch();
 			const page = await browser.newPage();
 
 			await page.goto('https://google.com/search?q=' + request.url);
 			var theHTML = await page.content();
 
-			fs.writeFile('./upcs' + request.url, theHTML, (err) => {
+			fs.writeFile('./upcs' + request.url + '.html', theHTML, (err) => {
 				// throws an error, you could also catch it here
 				if (err) throw err;
 
@@ -26,10 +27,8 @@ const requestHandler = (request, response) => {
 
 			browser.close();
 		}
+		run();
 	}
-
-	run();
-
 }
 
 const server = http.createServer(requestHandler)
@@ -39,5 +38,5 @@ server.listen(port, (err) => {
     return console.log('something bad happened', err);
   }
 
-  console.log('server is listening on ${port}');
+  console.log('server is listening on ' + port);
 })
