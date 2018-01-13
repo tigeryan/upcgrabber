68e9ba1f8f208374f8819decea5198d4075f7f28
John Ceci <tigeryan55@gmail.com>
2018-01-12 20:15:12 -0500

    initial

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+	console.log(request.url);
+	response.end('Hello Node.js Server! ' + request.url);
+
+	const puppeteer = require('puppeteer');
+
+	if (request.url != '/favicon.ico') {
+		async function run() {
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+
+			await page.goto('https://google.com/search?q=' + request.url);
+			var theHTML = await page.content();
+
+			fs.writeFile('./upcs' + request.url, theHTML, (err) => {
+				// throws an error, you could also catch it here
+				if (err) throw err;
+
+				// success case, the file was saved
+				console.log('UPC saved!');
+			});
+
+			browser.close();
+		}
+	}
+
+	run();
+
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log('server is listening on ${port}');
+})
