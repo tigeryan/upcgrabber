d7ad5f086f8e0589c0a2ec68da0b7274560f4af7
John Ceci <tigeryan55@gmail.com>
2018-01-13 21:30:41 -0500

    post html div to server for saving

--- index.js
+++ index.js
@@ -18,6 +18,7 @@
 const fs = require('fs');
 const http = require('http');
 const port = process.env.PORT || 5000;
+const request = require('request');
 
 const requestHandler = (request, response) => {
 
@@ -37,21 +38,50 @@ const requestHandler = (request, response) => {
 			await page.goto('https://google.com/search?q=' + request.url);
 			var theHTML = await page.content();
 
-			console.log(theHTML.substring(1,100);
 			
+			//jackpot-main-content-container
+			//097855056979l
+			//console.log(theHTML.substring(1,100));
+			
+			var returnRequest = require('request');
+
+			//const textContent = await page.evaluate(() => document.querySelector('.jackpot-main-content-container').textContent);
+			//const innerText = await page.evaluate(() => document.querySelector('.jackpot-main-content-container').innerText);
+			const innerHTML = await page.evaluate(() => document.querySelector('.jackpot-main-content-container').innerHTML);
+
+			//console.log(textContent);
+			//console.log(innerText);
+			//console.log(innerHTML);
+			
+			let options = {  
+				url: 'http://www.njracing.com/receiveHTML.cfc?method=getHTML',
+				form: { theUPC: request.url, theHTML: innerHTML }
+			};			
+
+			//console.log( options );
+			
+			returnRequest.post(options,
+				function (error, returnResponse, body) {
+					if (!error && returnResponse.statusCode == 200) {
+						console.log(body)
+					}
+					//console.log(returnResponse);
+					console.log('sent');
+				}
+			);			
+			
+			/*
 			fs.writeFile( __dirname + '/upcs' + request.url + '.html', theHTML, {"encoding":'ascii'}, (err) => {
-				// throws an error, you could also catch it here
 				if (err) {
 					responseMsg = responseMsg + '<p>Error thrown</p>';
 					throw err;
 				}
 
-				
-				// success case, the file was saved
 				responseMsg = responseMsg + '<p>Page Saved</p>';
 				console.log('UPC saved!');
 			});
-
+			*/
+			
 			browser.close();
 		}
 		run();
